614c00c13e83b9a9d61113a289aa5a1a7b1dc029
lvann05 <nam856167@gmail.com>
2026-03-27 09:49:00 +0700

    remove target from git

--- src/main/java/poly/barber/util/XJdbc.java
+++ src/main/java/poly/barber/util/XJdbc.java
@@ -25,7 +25,7 @@ public class XJdbc {
         var driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
         var dburl = "jdbc:sqlserver://localhost:1433;databaseName=Barber5AE;encrypt=true;trustServerCertificate=true;";
         var username = "sa";
-        var password = "123";
+        var password = "123443211234@";
         try {
             if (!XJdbc.isReady()) {
                 Class.forName(driver);
