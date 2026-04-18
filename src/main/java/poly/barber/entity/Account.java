ed053cd915d224fa072ba7d772e67d851f5fb92f
sapogcacvaza <vuthanhlong909@gmail.com>
2026-04-18 09:01:04 +0700

    Chỉnh lombok

--- src/main/java/poly/barber/entity/Account.java
+++ src/main/java/poly/barber/entity/Account.java
@@ -10,26 +10,28 @@ import lombok.NoArgsConstructor;
 @Builder
 @Data
 public class Account {
+
     private int accountID;
     private String username;
     private String password;
     private int role; // 🔥 THÊM CÁI NÀY
     private int employeeID;
-public String getRoleName() {
-    switch (this.role) {
-        case 1:
-            return "Admin";
-        case 2:
-            return "Quản lý";
-        case 3:
-            return "Nhân viên";
-        default:
-            return "Không xác định";
-    }
-}
 
-@Override
-public String toString() {
-    return username + " (" + getRoleName() + ")";
-}
+    public String getRoleName() {
+        switch (this.role) {
+            case 1:
+                return "Admin";
+            case 2:
+                return "Quản lý";
+            case 3:
+                return "Nhân viên";
+            default:
+                return "Không xác định";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return username + " (" + getRoleName() + ")";
+    }
 }
